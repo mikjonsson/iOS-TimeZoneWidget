303ae53d1f8985b4ae20b5479f97993b3eb7908f
Mikael Jonsson <mikjonsson@users.noreply.github.com>
2016-06-22 21:31:24 +1000

    Changed how selected timezones are shown in main app

--- TimeZoneWidget/ViewController.swift
+++ TimeZoneWidget/ViewController.swift
@@ -14,6 +14,16 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBOutlet weak var clockSwitch: UISwitch!
     @IBOutlet weak var daySwitch: UISwitch!
     
+    @IBOutlet weak var tzName1: UILabel!
+    @IBOutlet weak var tzName2: UILabel!
+    @IBOutlet weak var tzName3: UILabel!
+    @IBOutlet weak var tzName4: UILabel!
+    
+    @IBOutlet weak var tz1: UILabel!
+    @IBOutlet weak var tz2: UILabel!
+    @IBOutlet weak var tz3: UILabel!
+    @IBOutlet weak var tz4: UILabel!
+    
     var selectedZones: [String?]!
     let zoneCellIdentifier = "TimeZoneCell"
     var timeZones = NSTimeZone.knownTimeZoneNames()
@@ -22,6 +32,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        timeZones.sortInPlace()
         
         // Hide 4th timezone if device is narrower than 375 (currently older device than iPhone 6)
         if (screenWidth < 375) {
@@ -52,19 +64,19 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         if (defaults!.objectForKey("tz1") != nil) {
             selectedZones[0] = defaults!.objectForKey("tz1") as? String
         } else {
-            selectedZones[0] = timeZones[0]
+            selectedZones[0] = "Asia/Bangkok"
             defaults!.setValue(selectedZones[0], forKey: "tz1")
         }
         if (defaults!.objectForKey("tz2") != nil) {
             selectedZones[1] = defaults!.objectForKey("tz2") as? String
         } else {
-            selectedZones[1] = timeZones[1]
+            selectedZones[1] = "Australia/Perth"
             defaults!.setValue(selectedZones[1], forKey: "tz2")
         }
         if (defaults!.objectForKey("tz3") != nil) {
             selectedZones[2] = defaults!.objectForKey("tz3") as? String
         } else {
-            selectedZones[2] = timeZones[2]
+            selectedZones[2] = "Europe/London"
             defaults!.setValue(selectedZones[2], forKey: "tz3")
         }
         
@@ -72,81 +84,58 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             if (defaults!.objectForKey("tz4") != nil) {
                 selectedZones[3] = defaults!.objectForKey("tz4") as? String
             } else {
-                selectedZones[3] = timeZones[3]
+                selectedZones[3] = "America/Detriot"
                 defaults!.setValue(selectedZones[3], forKey: "tz4")
             }
         }
         
-        sortAndCheck()
+        tzName1.text = selectedZones[0]!.location()
+        tzName2.text = selectedZones[1]!.location()
+        tzName3.text = selectedZones[2]!.location()
+        tzName4.text = selectedZones[3]!.location()
+        
+        updateTime()
     }
 
     
-    
     func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [UITableViewRowAction]? {
         let select1 = UITableViewRowAction(style: .Normal, title: "1") { action, index in
-            print("1: \(self.timeZones[indexPath.row])")
-            
+//            print("1: \(self.timeZones[indexPath.row])")
             self.selectedZones[0] = self.timeZones[indexPath.row]
             self.defaults!.setValue(self.selectedZones[0], forKey: "tz1")
-
-            self.sortAndCheck()
-            
+            self.tzName1.text = self.selectedZones[0]!.location()
             tableView.reloadData()
         }
         
         let select2 = UITableViewRowAction(style: .Normal, title: "2") { action, index in
-            print("2: \(self.timeZones[indexPath.row])")
-            
+//            print("2: \(self.timeZones[indexPath.row])")
             self.selectedZones[1] = self.timeZones[indexPath.row]
             self.defaults!.setValue(self.selectedZones[1], forKey: "tz2")
-
-            self.sortAndCheck()
-            
+            self.tzName2.text = self.selectedZones[1]!.location()
             tableView.reloadData()
         }
         
         let select3 = UITableViewRowAction(style: .Normal, title: "3") { action, index in
-            print("3: \(self.timeZones[indexPath.row])")
-            
+//            print("3: \(self.timeZones[indexPath.row])")
             self.selectedZones[2] = self.timeZones[indexPath.row]
             self.defaults!.setValue(self.selectedZones[2], forKey: "tz3")
-
-            self.sortAndCheck()
-            
+            self.tzName3.text = self.selectedZones[2]!.location()
             tableView.reloadData()
         }
         
-        
         if (screenWidth >= 375) {
             let select4 = UITableViewRowAction(style: .Normal, title: "4") { action, index in
-                print("4: \(self.timeZones[indexPath.row])")
-                
+//                print("4: \(self.timeZones[indexPath.row])")
                 self.selectedZones[3] = self.timeZones[indexPath.row]
                 self.defaults!.setValue(self.selectedZones[3], forKey: "tz4")
-                
-                self.sortAndCheck()
-                
+                self.tzName4.text = self.selectedZones[3]!.location()
                 tableView.reloadData()
             }
+            updateTime()
             return [select4, select3, select2, select1]
         } else {
+            updateTime()
             return [select3, select2, select1]
-        }
-    }
-    
-    
-    func sortAndCheck() {
-        timeZones.sortInPlace()
-        timeZones.removeAtIndex(timeZones.indexOf(selectedZones[0]!)!)
-        timeZones.insert(selectedZones[0]!, atIndex: 0)
-        timeZones.removeAtIndex(timeZones.indexOf(selectedZones[1]!)!)
-        timeZones.insert(selectedZones[1]!, atIndex: 1)
-        timeZones.removeAtIndex(timeZones.indexOf(selectedZones[2]!)!)
-        timeZones.insert(selectedZones[2]!, atIndex: 2)
-        
-        if (screenWidth >= 375) {
-            timeZones.removeAtIndex(timeZones.indexOf(selectedZones[3]!)!)
-            timeZones.insert(selectedZones[3]!, atIndex: 3)
         }
     }
     
@@ -163,6 +152,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             defaults!.setBool(false, forKey: "clockSwitchState")
         }
         defaults!.synchronize()
+        updateTime()
     }
     @IBAction func dayModeSwitch(sender: AnyObject) {
         if daySwitch.on {
@@ -171,6 +161,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             defaults!.setBool(false, forKey: "daySwitchState")
         }
         defaults!.synchronize()
+        updateTime()
     }
     
     
@@ -186,23 +177,68 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(zoneCellIdentifier, forIndexPath: indexPath) as! TimeZonesTableViewCell
         
-        if (screenWidth >= 375) {
-            if (indexPath.row > 3) {
-                cell.accessoryType = .None
+        cell.textLabel?.text = "\(timeZones[indexPath.row]) (\( (NSTimeZone(name: timeZones[indexPath.row])?.localizedName(NSTimeZoneNameStyle.ShortStandard, locale: NSLocale.currentLocale()))! as String ))"
+        
+        cell.layoutIfNeeded()
+        updateTime()
+        return cell
+    }
+    
+    func updateTime() {
+        var clockFormat = "HH:mm"
+        var dateFormat = "EEE"
+        
+        if (defaults!.objectForKey("clockSwitchState") != nil) {
+            if (defaults!.boolForKey("clockSwitchState")) {
+                clockFormat = "HH:mm"
             } else {
-                cell.accessoryType = .Checkmark
-            }
-        } else {
-            if (indexPath.row > 2) {
-                cell.accessoryType = .None
-            } else {
-                cell.accessoryType = .Checkmark
+                clockFormat = "h:mm a"
             }
         }
+        if (defaults!.objectForKey("daySwitchState") != nil) {
+            if (defaults!.boolForKey("daySwitchState")) {
+                dateFormat = "dd"
+            } else {
+                dateFormat = "EEE"
+            }
+        }
+      
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "\(dateFormat)\n\(clockFormat)"
         
-        cell.textLabel?.text = "\(timeZones[indexPath.row]) (\( (NSTimeZone(name: timeZones[indexPath.row])?.localizedName(NSTimeZoneNameStyle.ShortStandard, locale: NSLocale.currentLocale()))! as String ))"
-        cell.layoutIfNeeded()
+        formatter.timeZone = NSTimeZone(name: selectedZones[0]!)
+        tz1.text = formatter.stringFromDate(NSDate())
+        formatter.timeZone = NSTimeZone(name: selectedZones[1]!)
+        tz2.text = formatter.stringFromDate(NSDate())
+        formatter.timeZone = NSTimeZone(name: selectedZones[2]!)
+        tz3.text = formatter.stringFromDate(NSDate())
+        formatter.timeZone = NSTimeZone(name: selectedZones[3]!)
+        tz4.text = formatter.stringFromDate(NSDate())
         
-        return cell
+        // Hide 4th timezone if device width is less than 375 (currently older device than iPhone 6)
+        let screenWidth = UIScreen.mainScreen().bounds.width
+        if (screenWidth < 375) {
+            tzName4.hidden = true
+            tz4.hidden = true
+        }
+    }
+}
+
+
+// Fetch area or location of the timezone, and return full timezone name if it fails
+extension String
+{
+    public func area() -> String? {
+        if let i = self.characters.indexOf("/") {
+            return self.substringToIndex(i)
+        }
+        return self
+    }
+    
+    public func location() -> String? {
+        if let i = self.characters.indexOf("/") {
+            return self.substringFromIndex(i.successor())
+        }
+        return self
     }
 }
